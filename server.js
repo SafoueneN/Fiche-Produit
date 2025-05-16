fffebda4817e26ce440721502fdf686fdd4e0ddf
Safouen Saassouki <saassouki.safouene@esprit.tn>
2025-05-16 16:38:14 +0100

    🚀 Premier commit du projet Fiche-Produit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PDFDocument = require('pdfkit');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.set('view engine', 'ejs');
+app.set('views engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
